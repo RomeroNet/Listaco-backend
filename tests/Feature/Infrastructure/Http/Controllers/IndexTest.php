50f1f70a8f526606656aaa93dc0f1645799fb2f6
Francisco Jose Romero Medina <f.romeromedina@gmail.com>
2024-11-21 03:26:43 +0100

    Move API routes to /api

--- tests/Feature/Infrastructure/Http/Controllers/IndexTest.php
+++ tests/Feature/Infrastructure/Http/Controllers/IndexTest.php
@@ -5,6 +5,6 @@ use function Pest\Laravel\getJson;
 const EXPECTED_CONTENT = ['status' => 'RomeroNet Boilerplate :)'];
 
 it('should return some content', function () {
-    getJson('/')->assertContent(json_encode(EXPECTED_CONTENT))
+    getJson('/api')->assertContent(json_encode(EXPECTED_CONTENT))
         ->assertStatus(200);
 });
